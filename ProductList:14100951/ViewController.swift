b09416a28232016b96f93baac04268839d04c780
Remi.UHCS <remstarcullen@yahoo.com>
2016-12-31 12:39:08 +0000

    SaveName method used to add the managed object to the core data control

--- ProductList:14100951/ViewController.swift
+++ ProductList:14100951/ViewController.swift
@@ -16,8 +16,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
-    @IBAction func addItem(_ sender: UIButton) {
+    var products = [NSManagedObject]() //Core data object array that will load and save items
+    
+    @IBAction func addItem(_ sender: UIButton)
+    {
+        
     }
+    
+    
+    func saveName(name: String) {  //coreData function to save a name with the value name which is already set in the coreData entity
+        //call on the app delegate to accept data
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Item", in: managedContext) //defining entity
+        
+        let item = NSManagedObject(entity: entity!, insertInto: managedContext)//item object
+        
+        item.setValue(name, forKey: "name") //assigning the value to the key
+        do {
+            try managedContext.save()
+            products.append(item)
+        }
+        catch let error as NSError {
+            print(error)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "My Shopping List"
