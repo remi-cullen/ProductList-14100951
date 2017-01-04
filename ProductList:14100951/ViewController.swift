f06274d387f34134b66333510b7925ab7539a77b
Remi.UHCS <remstarcullen@yahoo.com>
2017-01-04 23:15:18 +0000

    Added autocompletion feature on most common items , if an item is not on the list the system will append the array to include the item

--- ProductList:14100951/ViewController.swift
+++ ProductList:14100951/ViewController.swift
@@ -9,13 +9,49 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
 
     
+    @IBOutlet weak var secondTableView: UITableView!
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var textField: UITextField!
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    /*AUTOCOMPLETE SECTION*/
+   var autoCompleteWords = ["Apple", "Banana","Bread","Cake","Cookies","Crackers","Cucumber","Dates","Eggs","Milk","Cheese","Ham","Butter","Oranges","Tuna","Tomatoes","Chicken","Sauce","Noodles","Leeks","Salt","Sugar","Flour","Strawberrys","Lettuce","Spices","Bacon","Sweets"]
+    var autoComplete = [String]()
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let subString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        
+        
+        
+        searchThroughAutoCompleteWithSubstring(subString:subString)
+        
+        return true
+    }
+    
+    func searchThroughAutoCompleteWithSubstring(subString: String)
+    {
+        autoComplete.removeAll(keepingCapacity: false)
+        
+        for key in autoCompleteWords
+        {
+            let myString: NSString! = key as NSString
+            let stringRange: NSRange! = myString.range(of: subString)
+            
+            if (stringRange.location == 0)
+            {
+                autoComplete.append(key)
+            }
+        }
+        secondTableView.reloadData()
+    }
+    /*************************************/
+    
     var products = [NSManagedObject]() //Core data object array that will load and save items
     
     @IBAction func addItem(_ sender: UIButton)
@@ -25,18 +61,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             self.tableView.dataSource = self
             let itemName = textField.text!
             self.saveName(name: itemName)
-            //let alert = UIAlertController(title: "New Product", message: "\(itemName) added to your shopping list", preferredStyle: .alert)
-            //let ok = UIAlertAction(title: "OK", style: .default, handler:
-                //{(action: UIAlertAction) -> Void in
-            
-                  //  print("\(itemName) added to your shopping list")
-                //})
+           
             self.tableView.reloadData()
             textField.text = ""
             textField.resignFirstResponder()
-            
-       // alert.addAction(ok)
-        //present(alert, animated: true, completion: nil)
+  
         
         }
     
@@ -68,10 +97,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
+        if tableView == self.secondTableView {
+            return autoComplete.count
+        }
+        
         return products.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView == self.secondTableView{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "aCell", for: indexPath)
+            let index = indexPath.row as Int
+            cell.textLabel?.text = autoComplete[index]
+            return cell
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let item = products[indexPath.row]
         cell.textLabel?.text = item.value(forKey: "name") as? String
@@ -81,38 +122,46 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     //checkmarks
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        let thisRow = tableView.cellForRow(at: indexPath)!
-        
-        if thisRow.accessoryType == UITableViewCellAccessoryType.none
+        if tableView == self.secondTableView
         {
-            thisRow.accessoryType = UITableViewCellAccessoryType.checkmark
-            thisRow.tintColor = UIColor.blue
-            let itemName = thisRow.textLabel?.text
-            let alert = UIAlertController(title: "New Product", message: "\(itemName!) added to your shopping cart", preferredStyle: .alert)
-            let ok = UIAlertAction(title: "OK", style: .default, handler:
-            {(action: UIAlertAction) -> Void in
+            let selectedCell: UITableViewCell = secondTableView.cellForRow(at: indexPath)!
+            textField.text = selectedCell.textLabel!.text!
             
-              print("\(itemName! ) added to your shopping cart")
-            })
-             alert.addAction(ok)
-            present(alert, animated: true, completion: nil)
-         }
-        else
-        {//also if the item is already oin the shopping cart , stop the adding of a new one
-            thisRow.accessoryType = UITableViewCellAccessoryType.none
+            
         }
+        else
+        {
+            let thisRow = tableView.cellForRow(at: indexPath)!
         
+            if thisRow.accessoryType == UITableViewCellAccessoryType.none
+            {
+                thisRow.accessoryType = UITableViewCellAccessoryType.checkmark
+                thisRow.tintColor = UIColor.blue
+                let itemName = thisRow.textLabel?.text
+                let alert = UIAlertController(title: "New Product", message: "\(itemName!) added to your shopping cart", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "OK", style: .default, handler:
+                    {(action: UIAlertAction) -> Void in
+            
+                        print("\(itemName! ) added to your shopping cart")
+                })
+                alert.addAction(ok)
+                present(alert, animated: true, completion: nil)
+            }
+            else
+            {
+                thisRow.accessoryType = UITableViewCellAccessoryType.none
+            }
+        }
     }
     //delete
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
     {
+        
         print("Deleted")
         let deletedRow = tableView.cellForRow(at: indexPath)!
         if editingStyle == UITableViewCellEditingStyle.delete
         {
             //Delete from coreData
-            //let appDelegate = UIApplication.shared.delegate as! AppDelegate
-            
             let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
             do {
                 try managedContext.delete(products[indexPath.row])
@@ -124,8 +173,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 print(error)
             }
             tableView.reloadData()
-            
-            
         }
     }
     
@@ -136,7 +183,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         title = "My Shopping List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        
+        self.secondTableView.delegate = self
+        self.textField.delegate = self
+        secondTableView.register(UITableViewCell.self, forCellReuseIdentifier: "aCell")
         
         let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
